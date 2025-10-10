f9f42660e78d98490d6a356ef6215b9dd1e4ddbf
Saifullahomar786 <Saifullahomar786@gmail.com>
2025-10-10 11:33:20 +0500

    Configure for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? '/Recovery-Protocol/' : '/',
   server: {
     port: 5173,
     open: true
